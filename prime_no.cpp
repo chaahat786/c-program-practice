30770b1e2443fded6df776496663483513694f48
Priyanka <25priyanka1998@gmmail.com>
2019-10-01 12:52:36 +0530

    learn alphabet print

---
+++ prime_no.cpp
@@ -0,0 +1,16 @@
+//repeat pppp
+#include <stdio.h>
+int main()
+{
+	int i, n;
+	printf("Enter a number");
+	scanf("%d", &n);
+	//		for(i=2;i<=n-1;i=i+2)
+
+	if (i % 2 == 0)
+	{
+
+		printf("%d", i);
+	}
+}
+// Error
---
+++ two_no.cpp
@@ -0,0 +1,10 @@
+//repeat two number
+#include <stdio.h>
+int main()
+{
+	int i;
+	for (i; i <= 10; ++i)
+	{
+		printf("%d", 90);
+	}
+}
